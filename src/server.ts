3a4b99cafd195224d696d4ca00da74907cadda49
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-02-22 14:04:58 +0900

    BRR:Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+// console.log("Exucuted");
+// // const person:string="Ethan";
+// // const count:number=100;
+// import moment from "moment";
+
+// const currentTime = moment().format("YYYY,MM,DD")
+// console.log(currentTime);
+
