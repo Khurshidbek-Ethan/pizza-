baffc9e41bb8e8555532e31966be566b00208f86
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-02-24 21:38:43 +0900

    feat:added traints

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+
+function gerReverse(str) {
+    // Stringni massivga aylantiramiz
+    let strArray = str.split('');
+    
+    // Massivni teskari tartibda qaytaramiz
+    let reversedArray = strArray.reverse();
+    
+    // Massivni qaytarilgan stringga aylantiramiz
+    let reversedStr = reversedArray.join('');
+    
+    return reversedStr;
+  }
+  
+  // Test qilamiz
+  var result = gerReverse("hello");
+  console.log(result); // Natija: "olleh"
