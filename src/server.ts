10f1f30744e181ae4902a1c655e039cc0bd2dece
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-02-24 14:59:48 +0900

    feat:connection mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,15 @@
-// console.log("Exucuted");
-// // const person:string="Ethan";
-// // const count:number=100;
-// import moment from "moment";
-
-// const currentTime = moment().format("YYYY,MM,DD")
-// console.log(currentTime);
-
-///////////////////////////////////////
-
 import dotenv from 'dotenv';
 dotenv.config();
-// console.log("PORT:",process.env.PORT); 
 
-// console.log("MONGO_URL:",process.env.MONGO_URL); 
+import mongoose from "mongoose";
 
+mongoose.connect(process.env.MONGO_URL as string,{})
+.then((data)=>{
+    console.log("MongoDB connection succed");
+    const PORT = process.env.PORT ?? 3003;
+    
+})
+.catch((err)=> console.log("ERROR on connection MongoDB",err));
 
 
 
