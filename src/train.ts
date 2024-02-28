3a1e8a0b993cd9f70bd596910016a77155b8476f
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-02-28 15:35:28 +0900

    feat: schema model

--- src/train.ts
+++ src/train.ts
@@ -1,17 +1,30 @@
+function getPositive(arr) {
+  let positiveNums = arr.filter( num > 0);
+  let result = positiveNums.length > 0 ? positiveNums.join('') : '';
+  return result;
+}
 
-function gerReverse(str) {
-    // Stringni massivga aylantiramiz
-    let strArray = str.split('');
+// Test
+console.log(getPositive([1, -4, 2])); // Output: "12"
+
+
+
+
+
+
+// function gerReverse(str) {
+//     // Stringni massivga aylantiramiz
+//     let strArray = str.split('');
     
-    // Massivni teskari tartibda qaytaramiz
-    let reversedArray = strArray.reverse();
+//     // Massivni teskari tartibda qaytaramiz
+//     let reversedArray = strArray.reverse();
     
-    // Massivni qaytarilgan stringga aylantiramiz
-    let reversedStr = reversedArray.join('');
+//     // Massivni qaytarilgan stringga aylantiramiz
+//     let reversedStr = reversedArray.join('');
     
-    return reversedStr;
-  }
+//     return reversedStr;
+//   }
   
-  // Test qilamiz
-  var result = gerReverse("hello");
-  console.log(result); // Natija: "olleh"
+//   // Test qilamiz
+//   var result = gerReverse("hello");
+//   console.log(result); // Natija: "olleh"
