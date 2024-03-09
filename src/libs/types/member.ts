457b6ebd294e66fcec036c21b8571401e7ab650e
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-03-09 18:54:21 +0900

    fix:create sessions on login and signup process

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -1,6 +1,7 @@
 import { MemberStatus, MemberType } from "../enums/member.enum";
 import {ObjectId} from "mongoose";
-
+import {Request} from "express";
+import {Session} from "express-session";
 
 export interface Member {
     _id: ObjectId;
@@ -35,4 +36,9 @@ export interface MemberInput{
 export interface LoginInput{
     memberNick:string;
     memberPassword:string; 
+}
+
+export interface AdminRequest extends Request{
+    member:Member,
+    session:Session & { member:Member }
 }
