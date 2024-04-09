b11e866ec1509d30269816f11f8bd18189b33a22
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-04-09 18:39:24 +0900

    feat: develop update member

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,26 @@
 import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
-
+import uploader from "./libs/utils/uploader";
+ 
 
        // react
        /** Member **/
 router.post('/member/login',memberController.login);
 router.post("/member/signup",memberController.signup);
-router.post("/member/logout",memberController.veryfyAuth,memberController.logout);
-router.get("/member/detail", memberController.veryfyAuth, memberController.getMemberDetail);
-
+router.post("/member/logout",
+memberController.verifyAuth,
+memberController.logout
+);
+router.get("/member/detail",
+ memberController.verifyAuth,
+  memberController.getMemberDetail
+  );
+router.post("/member/update",
+memberController.verifyAuth, 
+uploader("members").single("memberImage"),
+memberController.updateMember
+);
 
 
        /** Product **/
