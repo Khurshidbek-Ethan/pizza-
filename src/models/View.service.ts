e8d46cb40fb2539335ecafb0c6a9060be93b53c9
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-04-12 14:30:55 +0900

    fix: modify getProduct busness logic

--- src/models/View.service.ts
+++ src/models/View.service.ts
@@ -1,4 +1,7 @@
+import Errors, { HttpCode, Message } from "../libs/Errors";
+import { View, ViewInput } from "../libs/types/view";
 import ViewModel from "../schema/View.model";
+
 
 
 
@@ -7,6 +10,21 @@ class ViewService {
 
   constructor() {
     this.viewModel = ViewModel;
+  }
+ 
+  public async checkViewExistence(input: ViewInput): Promise<View> {
+   return  await this.viewModel.findOne({memberId: input.memberId, viewRefId: input.viewRefId})
+   .exec();
+  }
+
+  public async insertMemberView(input:ViewInput): Promise<View> {
+    try{
+     return await this.viewModel.create(input);
+    } catch (err) {
+        console.log("ERROR, model:insertMemberView",err);
+        throw new Errors (HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+        
+    }
   }
 
 }
