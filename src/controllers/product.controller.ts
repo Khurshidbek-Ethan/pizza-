9746d3566c8733d92d1acccea2a235e16fa2e244
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-04-12 12:16:39 +0900

    feat:develop getProduct Resr Api

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,7 +3,7 @@ import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
 import { ProductInput, ProductInquiry } from "../libs/types/product";
-import { AdminRequest } from "../libs/types/member"; 
+import { AdminRequest, ExtendedRequest } from "../libs/types/member"; 
 import { ProductCollection } from "../libs/enums/product.enum";
 
 
@@ -51,6 +51,30 @@ const  productController:T = {};
       else res.status(Errors.statndard.code).json(Errors.statndard);
     }
   }
+
+
+   
+   productController.getProduct = async (req:ExtendedRequest, res:Response) => {
+    try{
+      console.log('getProduct');
+      // const id: string = req.params.id;
+      const {id} = req.params;
+      console.log("req.member:",req.member);
+      
+      const memberId = req.member?._id ?? null,
+         result = await productService.getProduct(memberId, id);
+  
+     
+     
+      res.status(HttpCode.OK).json(result);
+    }catch(err){
+     console.log("Error,getProduct:",err);
+     if(err instanceof Errors)res.status(err.code).json(err);
+      else res.status(Errors.statndard.code).json(Errors.statndard);
+    }
+   } 
+
+
     
 
     /** SSR */
